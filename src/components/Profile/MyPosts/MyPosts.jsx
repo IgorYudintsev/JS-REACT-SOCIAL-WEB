5dc57682edaba59c830f6f36494b327b43fd8105
mobile08092018 <kakvospitat@gmail.com>
2020-07-11 20:25:03 +0300

    11.07.2020-onClick-props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
+import Profile from "../Profile";
+
 
 
 const MyPosts = (props) => {
@@ -8,7 +10,7 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef()
     let addPost = () => {
       let text=newPostElement.current.value;
-        alert(text)
+        props.addPosts(text);
     }
 
     return (
