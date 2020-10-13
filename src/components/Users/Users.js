88245ac2ce43b4a1f6dc07d227c76a182ca74131
mobile08092018 <kakvospitat@gmail.com>
2020-10-13 20:41:29 +0300

    13.10.2020-51 - React JS практика - Users API

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './Users.module.css'
+import * as axios from 'axios';
 
 export const Users = (props) => {
 
-    if(props.users.length===0){
-        props.setUser([
-            {id: 1, fullName: 'Dmitry',  photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: true, status: 'Im a boss', location: {city: 'Minsk', country: 'Belarus'}},
-            {id: 2, fullName: 'Igor', photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: true, status: 'Im a middle', location: {city: 'Moscor', country: 'Russia'}},
-            {id: 3, fullName: 'Sasha', photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: false, status: 'Im a june', location: {city: 'Kiev', country: 'Ukrain'}},
-        ])
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUser(response.data.items)
+            console.log(response)
+        })
     }
 
 
@@ -18,7 +18,7 @@ export const Users = (props) => {
                 <div key={m.id}>
             <span>
                 <div>
-                    <img src={m.photoUrl} className={styles.picture}/>
+                    <img src={m.photos.small!=null? m.photos.small:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png'} className={styles.picture}/>
                 </div>
                 <div>
                     {m.followed
@@ -27,13 +27,24 @@ export const Users = (props) => {
                     }
                 </div>
             </span>
-                    <span><div className={styles.marg}>{m.fullName}</div><div
-                        className={styles.marg}>{m.status}</div></span>
-                    <span><div className={styles.marg}>{m.location.country}</div><div
-                        className={styles.marg}>{m.location.city}</div></span>
+                    <span>
+                        <div className={styles.marg}>{m.name}</div>
+                        <div  className={styles.marg}>{m.status}</div>
+
+                    </span>
+                    <span>
+                        <div className={styles.marg}>{"m.location.country"}</div><div
+                        className={styles.marg}>{"m.location.city"}</div>
+                    </span>
                 </div>)}
         </div>
     )
 }
 
 export default Users;
+
+// [
+//     {id: 1, fullName: 'Dmitry',  photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: true, status: 'Im a boss', location: {city: 'Minsk', country: 'Belarus'}},
+//     {id: 2, fullName: 'Igor', photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: true, status: 'Im a middle', location: {city: 'Moscor', country: 'Russia'}},
+//     {id: 3, fullName: 'Sasha', photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: false, status: 'Im a june', location: {city: 'Kiev', country: 'Ukrain'}},
+// ]
