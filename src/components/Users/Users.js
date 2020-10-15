3f6389c45513a4d81d035bd5e5871f669a2a7eb5
mobile08092018 <kakvospitat@gmail.com>
2020-10-15 21:09:57 +0300

    15.10.2020-52 - React JS практика - Side Effect

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,22 +3,26 @@ import styles from './Users.module.css'
 import * as axios from 'axios';
 
 export const Users = (props) => {
-
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUser(response.data.items)
-            console.log(response)
-        })
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUser(response.data.items)
+                console.log(response)
+            })
+        }
     }
 
 
     return (
         <div>
+            <button onClick={getUsers}>GET USERS</button>
             {props.users.map(m =>
                 <div key={m.id}>
             <span>
                 <div>
-                    <img src={m.photos.small!=null? m.photos.small:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png'} className={styles.picture}/>
+                    <img
+                        src={m.photos.small != null ? m.photos.small : 'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png'}
+                        className={styles.picture}/>
                 </div>
                 <div>
                     {m.followed
@@ -29,7 +33,7 @@ export const Users = (props) => {
             </span>
                     <span>
                         <div className={styles.marg}>{m.name}</div>
-                        <div  className={styles.marg}>{m.status}</div>
+                        <div className={styles.marg}>{m.status}</div>
 
                     </span>
                     <span>
