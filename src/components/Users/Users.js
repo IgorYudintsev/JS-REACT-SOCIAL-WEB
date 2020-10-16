81411a8fff8bef7884aaa14cbe98d9e747ba2299
mobile08092018 <kakvospitat@gmail.com>
2020-10-16 19:51:48 +0300

    16.10.2020-53 - React JS практика - классовый компонент (extends React.Component)

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,22 +2,21 @@ import React from 'react';
 import styles from './Users.module.css'
 import * as axios from 'axios';
 
-export const Users = (props) => {
-    const getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUser(response.data.items)
-                console.log(response)
-            })
-        }
+class Users extends React.Component{
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUser(response.data.items)
+            console.log(response)
+        })
     }
 
+    render() {
+        return (
+            <div>
 
-    return (
-        <div>
-            <button onClick={getUsers}>GET USERS</button>
-            {props.users.map(m =>
-                <div key={m.id}>
+                {this.props.users.map(m =>
+                    <div key={m.id}>
             <span>
                 <div>
                     <img
@@ -26,29 +25,24 @@ export const Users = (props) => {
                 </div>
                 <div>
                     {m.followed
-                        ? <button onClick={() => props.unfollow(m.id)} className={styles.margForBtn}>UNFollow</button>
-                        : <button onClick={() => props.follow(m.id)} className={styles.margForBtn}>Follow</button>
+                        ? <button onClick={() => this.props.unfollow(m.id)} className={styles.margForBtn}>UNFollow</button>
+                        : <button onClick={() => this.props.follow(m.id)} className={styles.margForBtn}>Follow</button>
                     }
                 </div>
             </span>
-                    <span>
+                        <span>
                         <div className={styles.marg}>{m.name}</div>
                         <div className={styles.marg}>{m.status}</div>
 
                     </span>
-                    <span>
+                        <span>
                         <div className={styles.marg}>{"m.location.country"}</div><div
-                        className={styles.marg}>{"m.location.city"}</div>
+                            className={styles.marg}>{"m.location.city"}</div>
                     </span>
-                </div>)}
-        </div>
-    )
+                    </div>)}
+            </div>
+        )
+    }
 }
 
 export default Users;
-
-// [
-//     {id: 1, fullName: 'Dmitry',  photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: true, status: 'Im a boss', location: {city: 'Minsk', country: 'Belarus'}},
-//     {id: 2, fullName: 'Igor', photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: true, status: 'Im a middle', location: {city: 'Moscor', country: 'Russia'}},
-//     {id: 3, fullName: 'Sasha', photoUrl:'https://e7.pngegg.com/pngimages/613/636/png-clipart-computer-icons-user-profile-male-avatar-avatar-heroes-logo.png', followed: false, status: 'Im a june', location: {city: 'Kiev', country: 'Ukrain'}},
-// ]
