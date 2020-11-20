93c179dccef54d5582a445ea40d09e9aab3411ef
mobile08092018 <kakvospitat@gmail.com>
2020-11-20 17:24:43 +0300

    19.11.2020 61 - React JS - cookie, login в теории, auth/me

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -20,6 +20,10 @@ const authReducer=(state=initialState,action)=>{
 }
 
 
-export const setAuthUserData = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}  })
+export const setAuthUserData = (userId, email, login) => (
+    {
+        type: SET_USER_DATA,
+        data: {userId, email, login}
+    })
 export default authReducer;
 
